bf26f5131e7d2b13ef1e3d6e8f64a3e396db8f3f
Thai Quoc Cuong <thai.q.cuong97@gmail.com>
2020-07-22 10:56:01 +0700

    calculator #2

--- Javascripts/script.js
+++ Javascripts/script.js
@@ -1,4 +1,5 @@
 let inputShow = document.getElementById("result");
+let resetInput = document.getElementById("reset_input");
 
 function Reset() {
   inputShow.value = "";
@@ -7,10 +8,23 @@ function Reset() {
 function Delete() {
   inputShow.value = inputShow.value.substr(0, inputShow.value.length - 1);
 }
+
 function Equal() {
-  //  console.log(inputShow.value)
-  if (inputShow.value && inputShow.value === "undefined") {
+  if (inputShow.value === "" || inputShow.value === undefined) {
     inputShow.value = 0;
-    console.log('xx',inputShow.value)
-  } else inputShow.value = eval(inputShow.value);
+  } else {
+    inputShow.value = eval(inputShow.value);
+  }
+}
+
+/* chia cho 100 */
+function Percent() {
+  inputShow.value = inputShow.value / 100;
+}
+
+/* chuyển số âm sang dương và ngược lại */
+function ChangeValue() {
+  if (inputShow.value > 0) {
+    inputShow.value *= -1;
+  } else inputShow.value *= -1;
 }
